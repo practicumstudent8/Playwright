c9001082d279a3a25b0757a7d7ae6d39b03146bb
karthik kutala <kutalakarthik2gmail.com>
2025-04-29 21:40:17 -0600

    Add Playwright CI setup

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ const config=({
   use: {
 
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
